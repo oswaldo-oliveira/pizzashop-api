793e86ae18d6407bfa036954483c519689638c5b
Oswaldo Oliveira <oswaldo.dev.oliveira@gmail.com>
2024-03-19 15:12:24 -0300

    :wrench: chore: update docker-compose and add eslint

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -1,7 +1,9 @@
 import type { Config } from "drizzle-kit";
+import { env } from "./src/env";
 
 export default {
   schema: "./src/db/schema/index.ts",
   out: "./drizzle",
   driver: 'pg',
+  dbCredentials: { connectionString: env.DATABASE_URL }
 } satisfies Config;
